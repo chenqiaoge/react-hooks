1bc5d3a89cdaab71282ab23b2d8cbeef29c261a8
陈桥格 <qiaoge.chen@ikang.com>
2020-11-09 19:25:55 +0800

    cancel on Drop Outside

--- src/views/dragDropDemo/dndComps/sortable/Container.jsx
+++ src/views/dragDropDemo/dndComps/sortable/Container.jsx
@@ -1,6 +1,8 @@
-import React,{ useState, useCallback} from 'react'
+import React, { useState, useCallback } from 'react'
+import { useDrop } from 'react-dnd'
 import update from 'immutability-helper'
 import { Card } from './Card'
+import { ItemTypes } from '../ItemTypes'
 
 const style = {
   width: 400,
@@ -38,28 +40,58 @@ const Container = () => {
     },
   ])
 
-  const moveCard = useCallback(
-    (dragIndex, hoverIndex) => {
-      const dragCard = cards[dragIndex]
-      setCards(
-        update(cards, {
-          $splice: [
-            [dragIndex, 1],
-            [hoverIndex, 0, dragCard],
-          ],
-        })
-      )
-    },
-    [cards]
-  )
+  // const moveCard = useCallback(
+  //   (dragIndex, hoverIndex) => {
+  //     const dragCard = cards[dragIndex]
+  //     setCards(
+  //       update(cards, {
+  //         $splice: [
+  //           [dragIndex, 1],
+  //           [hoverIndex, 0, dragCard],
+  //         ],
+  //       })
+  //     )
+  //   },
+  //   [cards]
+  // )
+
+  const moveCard = (id, atIndex) => {
+    const { card, index } = findCard(id)
+    setCards(
+      update(cards, {
+        $splice: [
+          [index, 1],
+          [atIndex, 0, card],
+        ],
+      })
+    )
+  }
+  // add 结束拖拽时，超出区域，要复原到初始位置
+  const findCard = (id) => {
+    const card = cards.filter((c) => c.id === id)[0]
+    return {
+      card,
+      index: cards.indexOf(card),
+    }
+  }
+  const [, drop] = useDrop({ accept: ItemTypes.CARD })
+
   const renderCard = (card, index) => {
     return (
-      <Card key={card.id} index={index} id={card.id} text={card.text} moveCard={moveCard}></Card>
+      <Card
+        key={card.id}
+        index={index}
+        id={card.id}
+        text={card.text}
+        moveCard={moveCard}
+        findCard={findCard}></Card>
     )
   }
   return (
     <>
-      <div style={style}>{cards.map((card, i) => renderCard(card, i))}</div>
+      <div ref={drop} style={style}>
+        {cards.map((card, i) => renderCard(card, i))}
+      </div>
     </>
   )
 }
